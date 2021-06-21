87332efe904a029c7c2665d3e14abbfdd48d7834
Ewanescence <ewanescence91@gmail.com>
2021-06-21 16:12:04 +0200

    feat: html, css & js

---
+++ main.js
@@ -0,0 +1,42 @@
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+
+var url = 'http://localhost:3000/api/teddies';
+var request = new XMLHttpRequest();
+request.open('GET', url);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var teddies = request.response;
+    listProducts(teddies);
+}
+
+function listProducts(jsonObj) {
+    
+    var ours = jsonObj;
+  
+    for (var i = 0; i < jsonObj.length; i++) {
+      var myA = document.createElement('a');
+      var myArticle = document.createElement('article');
+      var myFooter = document.createElement('footer');
+      var myImg = document.createElement('img');
+      var myH2 = document.createElement('h2');
+      var myPara1 = document.createElement('p');
+  
+      myA.href = '#';
+      myImg.src = ours[i].imageUrl;
+      myH2.textContent = ours[i].name;
+      myPara1.textContent = ours[i].price + " €";
+
+      myArticle.appendChild(myImg);
+      myFooter.appendChild(myH2);
+      myFooter.appendChild(myPara1);
+      myArticle.appendChild(myFooter);
+      myA.appendChild(myArticle);
+      section.appendChild(myA);
+        
+      }
+    }
+
+    
